b59baaf9f5d88a0d6c99dc1f5797e3369aed6a72
Raul Luis <soyrauluis@gmail.com>
2022-06-08 18:26:21 -0400

    Create Loan model

---
+++ app/src/main/java/com/devaruluis/primerparcial/model/Loan.kt
@@ -0,0 +1,15 @@
+package com.devaruluis.primerparcial.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "loans")
+data class Loan(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    @ColumnInfo(name = "debtorName")
+    val debtorName: String?,
+    @ColumnInfo(name = "amount")
+    val amount: Float?,
+)
